b6f88f4f0a40fe0e2d1c85b07d5022afcea51ff5
罗磊 <luolei@guaohao.com>
2017-05-15 15:37:16 +0800

    init springcloud project

---
+++ cloud-application-clients/cloud-application-clients-springboot/src/main/java/com/luolei/spring/could/client/springboot/ApplicationCaller.java
@@ -0,0 +1,45 @@
+package com.luolei.spring.could.client.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class ApplicationCaller {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ApplicationCaller.class)
+                .web(false)
+                .run(args);
+    }
+}
+
+@Component
+class RestTemplateExample implements CommandLineRunner {
+
+    @Autowired
+    private RestTemplate restTemplate;
+    private static final String GREETING_SERVICE_URI = "http://greeting.service/greeting/{name}"; 
+
+    @Override
+    public void run(String... strings) throws Exception {
+    	while(true) {
+    		String greetingSentence = this.restTemplate.getForObject(
+    				GREETING_SERVICE_URI, 
+    				String.class, 
+    				"Dean Shi");
+    		
+    		System.out.println("Response result: " + greetingSentence);
+    		
+    		Thread.sleep(5000);
+    	}
+    }
+
+}
